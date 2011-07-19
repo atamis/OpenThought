d4874ecc46027f3be02372ce810d37a9014cf4eb
Indigo Casson <atamiser@gmail.com>
2011-07-18 22:23:03 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.9'
+gem "rspec-rails", "2.6.1", :group => [:development, :test]
+gem "database_cleaner", "0.6.7", :group => :test
+gem "mongoid-rspec", "1.4.2", :group => :test
+gem "factory_girl_rails", "1.1.beta1", :group => :test
+gem "bson_ext", "1.3.1"
+gem "mongoid", "2.0.2"
+gem "devise", "1.3.4"
+gem "rails-footnotes", ">= 3.7", :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openthought::Application.config.secret_token = 'c3dccd9c4f0b0074103b033da215319ae8ee749f423f32b085b26bb89ceb0aca8e6e529faf0ccb0043efec9dd846f0134bee3253eb44d7fca7754b4ed8153b30'
