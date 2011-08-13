d31c8e586f90f5ee85cf73ce336ef5550b8f094f
Indigo Casson <atamiser@gmail.com>
2011-08-13 15:36:51 -0700

    The thoughts now arrive in the view sorted by updated at.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
     @users = User.all
-    @thoughts = current_user.thoughts
+    @thoughts = current_user.thoughts.desc(:updated_at)
   end
 end
