c92836348ad2797364bc7e72447308ad2dd90471
Andrew Amis <atamiser@gmail.com>
2011-09-01 16:41:49 -0700

    Added the Tags page, which lists tags.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @tags = Thought.all_tags(current_user)
+    @tags = Thought.weighted_tags(current_user)
 
     respond_to do |format|
-      format.html { render :text => @tags.join("\n") }
+      format.html
       format.xml { render :xml => @tags }
       format.json { render :json => @tags }
       format.text { render :text => @tags.join("\n") }
@@ -13,10 +13,12 @@ class TagsController < ApplicationController
 
   def show
     @thoughts = Thought.thoughts_for params[:tag], current_user
-    @tags = Thought.weighted_tags current_user
 
     respond_to do |format|
-      format.html { render "home/index" }
+      @tags = Thought.weighted_tags current_user
+      format.html do 
+        render "home/index"
+      end
       format.xml  { render :xml => @thoughts }
       format.json  { render :json => @thoughts }
       format.text  { render :text => @thoughts.map { |x| x.thought }.join("\n") }
