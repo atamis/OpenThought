ad4f17c7655f71de6035e0c272169924395b2009
Andrew Amis <atamiser@gmail.com>
2011-12-29 22:45:51 -0800

    Added thought model, hooked up relationship between thoughts and users.

---
+++ db/migrate/20111230063258_create_thoughts.rb
@@ -0,0 +1,10 @@
+class CreateThoughts < ActiveRecord::Migration
+  def change
+    create_table :thoughts do |t|
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
